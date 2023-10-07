2bc91e2ab2e35c6a32273eb0370f8fb57c7b817d
cadbit1510 <natthaphon.kkt@gmail.com>
2023-10-07 09:22:42 +0700

    currencyconverter

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+/*const BASE_URL = 'https://api.exchangeratesapi.io/v1/'*/
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  /*useEffect(()=> {
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  },[])*/
+
+  return (
+    
+    <>
+
+    </>
+    
+  )
+}
+
+export default App
